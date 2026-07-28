4d1fb13c836bf73a681e9e3cc7108e57f3eb9622
Rajan Kumar <rajankumar2003sknc@gmail.com>
2026-07-29 00:22:33 +0530

    Update BASE_URL to use VITE_BACKEND_URL in production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4000/api"
+    : import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // send cookies with the request
+  withCredentials: true,
 });
